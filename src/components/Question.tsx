dc0ee61d8305d54d500b434a7d249cb6b1f21ccc
jooheekim <jooheekim.dev@gmail.com>
2023-12-27 23:34:48 +0900

    [ FEAT ] : 설문 유형별 질문지 화면 구현
    Question 컴포넌트 유형별 화면 구현

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,26 +1,116 @@
-import React, { useRef } from 'react';
+import { useAppSelector, useAppDispatch } from '../app/hooks';
+import { selectQuestion } from '../redux/question/questionSlice';
+import { IoTrashOutline } from 'react-icons/io5';
+import { BsToggle2Off, BsToggle2On } from 'react-icons/bs';
+import { IoMdClose } from 'react-icons/io';
+import { IoMdCopy } from 'react-icons/io';
 import { useState } from 'react';
+import React from 'react';
 
 const Question = () => {
-  const [option, setOption] = useState();
   const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    console.log(e.target.value);
+    let selected = e.target.value;
   };
+  const questions = useAppSelector(selectQuestion);
+  console.log(questions);
 
   return (
-    <section className="shadow-md rounded-md p-3">
-      <div className="flex justify-between ">
-        <input type="text" name="question-title" className="block" />
-        <select className="block" onChange={handleSelect}>
-          <option value="short">단답형</option>
-          <option value="essay">장문형</option>
-          <option value="multiple">객관식</option>
-          <option value="checkbox">체크박스</option>
-          <option value="dropdown">드롭다운</option>
-        </select>
-      </div>
-      <input type="text" name="" id="" />
-    </section>
+    <>
+      <section className="p-4 m-2">
+        <div className="flex justify-between mb-2">
+          <input type="text" name="question-title" className="block" />
+          <select className="block" onChange={handleSelect}>
+            <option value="text">단답형</option>
+            <option value="textarea">장문형</option>
+            <option value="radio">객관식</option>
+            <option value="checkBox">체크박스</option>
+            <option value="dropdown">드롭다운</option>
+          </select>
+        </div>
+        <input type="text" className="bg-inherit" placeholder="단답형 텍스트" />
+        <div className="flex text-2xl">
+          <IoMdCopy />
+          <IoTrashOutline />
+          <BsToggle2Off />
+          <BsToggle2On />
+        </div>
+      </section>
+      <section className="p-4 m-2">
+        <div className="flex justify-between mb-2">
+          <input type="text" name="question-title" className="block" />
+          <select className="block" onChange={handleSelect}>
+            <option value="text">단답형</option>
+            <option value="textarea">장문형</option>
+            <option value="radio">객관식</option>
+            <option value="checkBox">체크박스</option>
+            <option value="dropdown">드롭다운</option>
+          </select>
+        </div>
+        <input type="text" name="" id="" placeholder="장문형" />
+        <div className="flex text-2xl">
+          <IoMdCopy />
+          <IoTrashOutline />
+          <BsToggle2Off />
+          <BsToggle2On />
+        </div>
+      </section>
+      <section className="p-4 m-2">
+        <div className="flex justify-between mb-2">
+          <input type="text" name="question-title" className="block" />
+          <select className="block" onChange={handleSelect}>
+            <option value="text">단답형</option>
+            <option value="textarea">장문형</option>
+            <option value="radio">객관식</option>
+            <option value="checkBox">체크박스</option>
+            <option value="dropdown">드롭다운</option>
+          </select>
+        </div>
+        <div className="mb-2">
+          <input type="radio" name="" id="" className="mr-2" />
+          <input type="text" name="" id="" className="bg-inherit border-b-2 " />
+          <IoMdClose className="inline text-xl" />
+        </div>
+        <div className="mb-2">
+          <input type="radio" name="" id="" className="mr-2" />
+          <input type="text" name="" id="" className="bg-inherit border-b-2 " />
+          <IoMdClose className="inline text-xl" />
+        </div>
+        <div className="flex text-2xl">
+          <IoMdCopy />
+          <IoTrashOutline />
+          <BsToggle2Off />
+          <BsToggle2On />
+        </div>
+      </section>
+      <section className="p-4 m-2">
+        <div className="flex justify-between mb-2">
+          <input type="text" name="question-title" className="block" />
+          <select className="block" onChange={handleSelect}>
+            <option value="text">단답형</option>
+            <option value="textarea">장문형</option>
+            <option value="radio">객관식</option>
+            <option value="checkBox">체크박스</option>
+            <option value="dropdown">드롭다운</option>
+          </select>
+        </div>
+        <div className="mb-2">
+          <input type="checkBox" name="" id="" className="mr-2" />
+          <input type="text" name="" id="" className="bg-inherit border-b-2 " />
+          <IoMdClose className="inline text-xl" />
+        </div>
+        <div className="mb-2">
+          <input type="checkBox" name="" id="" className="mr-2" />
+          <input type="text" name="" id="" className="bg-inherit border-b-2 " />
+          <IoMdClose className="inline text-xl" />
+        </div>
+        <div className="flex text-2xl">
+          <IoMdCopy />
+          <IoTrashOutline />
+          <BsToggle2Off />
+          <BsToggle2On />
+        </div>
+      </section>
+    </>
   );
 };
 
