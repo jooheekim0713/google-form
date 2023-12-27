57d8ab7a7d26c60fa1cbc1a953c6ad1fc08a898f
jooheekim <jooheekim.dev@gmail.com>
2023-12-27 15:47:33 +0900

    [ FEAT ] :  설문 문항 유형별 미리보기 화면 구현
     dropdown, radio 문항 유형별 미리보기 화면 기능 구현

--- src/pages/ViewForm.tsx
+++ src/pages/ViewForm.tsx
@@ -8,37 +8,63 @@ interface TitleProps {
   title?: { text: string; description: string };
 }
 
+const Question = () => {
+  const questions = useAppSelector(selectQuestion);
+
+  return (
+    <ol>
+      {questions.map((question) => (
+        <li className="p-4 shadow-md rounded-md m-2" key={question.id}>
+          <div className="mb-2 text-xl text">
+            <span>{question.required ? '* ' : ''}</span>
+            <span>{question.title}</span>
+          </div>
+
+          <div>
+            {question.type === 'dropdown' ? (
+              <select>
+                {question.answers.map((answer) => (
+                  <option value={answer}>{answer}</option>
+                ))}
+              </select>
+            ) : Array.isArray(question.answers) ? (
+              <ol>
+                {question.answers.map((answer, index) => (
+                  <li key={index}>
+                    <label className="block w-full">
+                      <input
+                        type={question.type}
+                        name={question.id.toString()}
+                        id={question.id.toString()}
+                      />
+                      <span>{answer}</span>
+                    </label>
+                  </li>
+                ))}
+              </ol>
+            ) : (
+              <textarea name="" id="" cols={50} rows={1} />
+            )}
+          </div>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
 const ViewForm = ({ title }: TitleProps) => {
   const subject = useAppSelector(selectTitle);
-  const questions = useAppSelector(selectQuestion);
-  console.log(questions);
 
+  //question.type이 'text'일 경우 길이 제한 'textarea'일경우 길게 작성할 수 있도록 만들것
   return (
     <div className="flex flex-col items-center">
       미리보기 화면입니다.
-      <div className="w-5/6 md:w-4/6 lg:w-1/2 min-w-min rounded-lg p-3 bg-slate-50">
+      <div className="w-5/6 md:w-4/6 lg:w-1/2 min-w-min rounded-lg bg-slate-50">
         <section>
           <h1 className="text-5xl">{subject.text}</h1>
           <h2 className="text-xl">{subject.description}</h2>
         </section>
-        <ol>
-          {questions.map((question) => (
-            <li className="" key={question.id}>
-              <div>{question.title}</div>
-              <div>{question.type}</div>
-              {Array.isArray(question.answers) ? (
-                <ol>
-                  {question.answers.map((answer, index) => (
-                    <li>{answer}</li>
-                  ))}
-                </ol>
-              ) : (
-                <div>{question.answers}</div>
-              )}
-              <div>{question.required.toString()}</div>
-            </li>
-          ))}
-        </ol>
+        <Question />
       </div>
     </div>
   );
--- src/redux/question/questionSlice.ts
+++ src/redux/question/questionSlice.ts
@@ -5,7 +5,7 @@ interface QuestionState {
   id: number;
   title: string;
   type: string;
-  answers: string[] | string;
+  answers: string[];
   required: boolean;
 }
 
@@ -22,8 +22,22 @@ const initialState: QuestionState[] = [
   {
     id: 1,
     title: '문항1입니다',
+    type: 'radio',
+    answers: ['첫번째 문항', '두번째 문항', '세번째 문항'],
+    required: true,
+  },
+  {
+    id: 2,
+    title: '문항2입니다',
+    type: 'dropdown',
+    answers: ['첫번째 문항', '두번째 문항', '세번째 문항'],
+    required: true,
+  },
+  {
+    id: 3,
+    title: '문항3입니다',
     type: 'short',
-    answers: '',
+    answers: [''],
     required: true,
   },
 ];
