0a3cf3bad1e29b7b88be2f9c0825470e7a1eb209
jooheekim <jooheekim.dev@gmail.com>
2023-12-28 15:21:05 +0900

    [ STYLE ] : 상단 헤더 CSS 수정
    Navbar 컴포넌트 CSS 수정

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { IoEyeOutline, IoEyeSharp } from 'react-icons/io5';
+import { IoEyeOutline } from 'react-icons/io5';
+import { CiViewList } from 'react-icons/ci';
 
 export default function Navbar() {
   const { pathname } = useLocation();
 
   return (
-    <header className=" ">
-      <div className="flex text-2xl justify-around">
-        <h1 className="">
-          {/** <img src="https://www.classum.com/images/Logo-type.png" /> - survey
-           */}
-        </h1>
-        <div>
+    <header className="text-2xl py-3">
+      <div className="flex flex-row items-center justify-evenly">
+        <h1 className="">JooheeKim's survey</h1>
+        <div className="p-2 rounded-full hover:text-brand hover:bg-slate-50">
           {pathname === '/' ? (
             <Link to="/viewForm">
               <IoEyeOutline />
             </Link>
           ) : (
             <Link to="/">
-              <IoEyeSharp />
+              <CiViewList />
             </Link>
           )}
         </div>
